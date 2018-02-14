758129cfc598055cbaa982334f4922502742c4f7
EM77120 <elnathan.mosimi@edu.ece.fr>
2018-02-14 13:17:41 +0100

    Salut

---
+++ blokus.h
@@ -0,0 +1,12 @@
+#ifndef BLOKUS_H_INCLUDED
+#define BLOKUS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <string.h>
+#include <conio.h>
+
+
+#endif // BLOKUS_H_INCLUDED
